37dc723c870ec8b1ef9b9365b99e893b551d484a
Elvio Ribeiro <elvioribeiro@msn.com>
2025-05-18 22:35:45 -0300

    new file:   frontend/imagens/dog.png

--- sql/init.sql
+++ sql/init.sql
@@ -1,11 +1,19 @@
+
 CREATE TABLE pets (
   id INT AUTO_INCREMENT PRIMARY KEY,
   nome VARCHAR(50),
-  foto VARCHAR(255)
+  foto TEXT
 );
-INSERT INTO pets (nome,foto) VALUES
-('Rex','/imagens/rex.jpg'),
-('Luna','/imagens/luna.jpg');
+
+INSERT INTO pets (nome, foto) VALUES
+  ('Rex', 'imagens/dog.png'),
+  ('Fido', 'imagens/dog.png'),
+  ('Max', 'imagens/dog.png'),
+  ('Lucy', 'imagens/dog.png'),
+  ('Milo', 'imagens/dog.png'),
+  ('Luna', 'imagens/dog.png');
+
+
 CREATE TABLE pedidos (
   id INT AUTO_INCREMENT PRIMARY KEY,
   pet_id INT,
